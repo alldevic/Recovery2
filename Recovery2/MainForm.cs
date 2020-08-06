81c74b04adcab2e51d76d5af5bd50d25963d8218
Nikolay Bely <belyynikolay@gmail.com>
2020-08-06 23:06:05 +0700

    Add base solution

---
+++ Recovery2/MainForm.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Forms;
+
+namespace Recovery2
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
