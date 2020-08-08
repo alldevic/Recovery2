5e8fa95f988bdcd3f2f6fbd4bd797ca4c7fd2343
Nikolay Bely <belyynikolay@gmail.com>
2020-08-08 22:11:26 +0700

    Add build deps, NLog, MainForm draft

--- Recovery2/MainForm.cs
+++ Recovery2/MainForm.cs
@@ -1,12 +1,33 @@
-﻿using System.Windows.Forms;
+﻿using System;
+using System.Windows.Forms;
+using Recovery2.Models;
 
 namespace Recovery2
 {
     public partial class MainForm : Form
     {
+        private User _user;
+
         public MainForm()
         {
             InitializeComponent();
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            _user = new User();
+        }
+        private void TextAge_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar))
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void ButtonClear_Click(object sender, EventArgs e)
+        {
+            _user = new User();
         }
     }
 }
---
+++ Recovery2/Models/User.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Recovery2.Models
+{
+    public class User
+    {
+        public User(string firstName, string secondtName, string lasttName, uint age, Gender gemder)
+        {
+            Gemder = gemder;
+            FirstName = firstName;
+            SecondtName = secondtName;
+            LasttName = lasttName;
+            Age = age;
+        }
+
+        public User()
+        {
+        }
+
+        public string FirstName { get; private set; } = string.Empty;
+        public string SecondtName { get; private set; } = string.Empty;
+        public string LasttName { get; private set; } = string.Empty;
+        public uint Age { get; private set; } = 0;
+        public Gender Gemder { get; private set; } = Gender.Male;
+        
+    }
+}
