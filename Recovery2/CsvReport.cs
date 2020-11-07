2e7bba89bf8e9d31e7815329eef0d74976a30ac9
Niikolay Bely <belyynikolay@gmail.com>
2020-11-07 07:53:01 +0700

    Correct NuGet dependencies

--- Recovery2/CsvReport.cs
+++ Recovery2/CsvReport.cs
@@ -15,8 +15,9 @@ namespace Recovery2
             {
                 using (var stream = File.Open(path, FileMode.Append))
                 using (var writer = new StreamWriter(stream, new UTF8Encoding(true)))
-                using (var csv = new CsvWriter(writer, CultureInfo.InvariantCulture))
+                using (var csv = new CsvWriter(writer))
                 {
+                    csv.Configuration.CultureInfo = CultureInfo.InvariantCulture;
                     csv.Configuration.HasHeaderRecord = false;
                     csv.Configuration.Delimiter = ";";
                     var lst = items.ToList();
@@ -30,8 +31,9 @@ namespace Recovery2
             }
 
             using (var writer = new StreamWriter(path, true, new UTF8Encoding(true)))
-            using (var csv = new CsvWriter(writer, CultureInfo.InvariantCulture))
+            using (var csv = new CsvWriter(writer))
             {
+                csv.Configuration.CultureInfo = CultureInfo.InvariantCulture;
                 csv.Configuration.Delimiter = ";";
                 csv.WriteRecords(items);
             }
