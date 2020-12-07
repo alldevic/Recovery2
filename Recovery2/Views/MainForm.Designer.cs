a081b329f7b768713a33b6f5008d3caabda95576
Niikolay Bely <belyynikolay@gmail.com>
2020-12-07 21:20:12 +0700

    Change MainForm font size to 12

--- Recovery2/Views/MainForm.Designer.cs
+++ Recovery2/Views/MainForm.Designer.cs
@@ -33,7 +33,6 @@ namespace Recovery2.Views
         /// </summary>
         private void InitializeComponent()
         {
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
             this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
             this.LabelLastName = new System.Windows.Forms.Label();
             this.LabelFirstName = new System.Windows.Forms.Label();
@@ -56,11 +55,11 @@ namespace Recovery2.Views
             // 
             this.tableLayoutPanel1.ColumnCount = 7;
             this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 96F));
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 96F));
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 96F));
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 96F));
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 96F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 120F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 120F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 120F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 120F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 120F));
             this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
             this.tableLayoutPanel1.Controls.Add(this.LabelLastName, 1, 1);
             this.tableLayoutPanel1.Controls.Add(this.LabelFirstName, 1, 2);
@@ -81,29 +80,28 @@ namespace Recovery2.Views
             this.tableLayoutPanel1.Name = "tableLayoutPanel1";
             this.tableLayoutPanel1.RowCount = 13;
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 26F));
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 26F));
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 26F));
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 26F));
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 26F));
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 26F));
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 26F));
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 26F));
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 26F));
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 26F));
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 26F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 30F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 30F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 30F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 30F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 30F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 30F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 30F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 30F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 30F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 30F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 30F));
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
             this.tableLayoutPanel1.Size = new System.Drawing.Size(784, 561);
             this.tableLayoutPanel1.TabIndex = 0;
             // 
             // LabelLastName
             // 
             this.LabelLastName.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.LabelLastName.Location = new System.Drawing.Point(155, 137);
+            this.LabelLastName.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte) (204)));
+            this.LabelLastName.Location = new System.Drawing.Point(95, 115);
             this.LabelLastName.Name = "LabelLastName";
-            this.LabelLastName.Size = new System.Drawing.Size(90, 26);
+            this.LabelLastName.Size = new System.Drawing.Size(114, 30);
             this.LabelLastName.TabIndex = 0;
             this.LabelLastName.Text = "Фамилия:";
             this.LabelLastName.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
@@ -111,9 +109,10 @@ namespace Recovery2.Views
             // LabelFirstName
             // 
             this.LabelFirstName.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.LabelFirstName.Location = new System.Drawing.Point(155, 163);
+            this.LabelFirstName.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte) (204)));
+            this.LabelFirstName.Location = new System.Drawing.Point(95, 145);
             this.LabelFirstName.Name = "LabelFirstName";
-            this.LabelFirstName.Size = new System.Drawing.Size(90, 26);
+            this.LabelFirstName.Size = new System.Drawing.Size(114, 30);
             this.LabelFirstName.TabIndex = 1;
             this.LabelFirstName.Text = "Имя:";
             this.LabelFirstName.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
@@ -121,9 +120,10 @@ namespace Recovery2.Views
             // LabelSecondName
             // 
             this.LabelSecondName.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.LabelSecondName.Location = new System.Drawing.Point(155, 189);
+            this.LabelSecondName.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte) (204)));
+            this.LabelSecondName.Location = new System.Drawing.Point(95, 175);
             this.LabelSecondName.Name = "LabelSecondName";
-            this.LabelSecondName.Size = new System.Drawing.Size(90, 26);
+            this.LabelSecondName.Size = new System.Drawing.Size(114, 30);
             this.LabelSecondName.TabIndex = 2;
             this.LabelSecondName.Text = "Отчество:";
             this.LabelSecondName.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
@@ -131,9 +131,10 @@ namespace Recovery2.Views
             // LabelAge
             // 
             this.LabelAge.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.LabelAge.Location = new System.Drawing.Point(155, 215);
+            this.LabelAge.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte) (204)));
+            this.LabelAge.Location = new System.Drawing.Point(95, 205);
             this.LabelAge.Name = "LabelAge";
-            this.LabelAge.Size = new System.Drawing.Size(90, 26);
+            this.LabelAge.Size = new System.Drawing.Size(114, 30);
             this.LabelAge.TabIndex = 3;
             this.LabelAge.Text = "Возраст:";
             this.LabelAge.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
@@ -141,9 +142,10 @@ namespace Recovery2.Views
             // LabelGender
             // 
             this.LabelGender.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.LabelGender.Location = new System.Drawing.Point(155, 241);
+            this.LabelGender.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte) (204)));
+            this.LabelGender.Location = new System.Drawing.Point(95, 235);
             this.LabelGender.Name = "LabelGender";
-            this.LabelGender.Size = new System.Drawing.Size(90, 26);
+            this.LabelGender.Size = new System.Drawing.Size(114, 30);
             this.LabelGender.TabIndex = 4;
             this.LabelGender.Text = "Пол:";
             this.LabelGender.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
@@ -152,50 +154,55 @@ namespace Recovery2.Views
             // 
             this.tableLayoutPanel1.SetColumnSpan(this.TextLastName, 2);
             this.TextLastName.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.TextLastName.Location = new System.Drawing.Point(251, 140);
+            this.TextLastName.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte) (204)));
+            this.TextLastName.Location = new System.Drawing.Point(215, 118);
             this.TextLastName.MaxLength = 50;
             this.TextLastName.Name = "TextLastName";
-            this.TextLastName.Size = new System.Drawing.Size(186, 20);
+            this.TextLastName.Size = new System.Drawing.Size(234, 26);
             this.TextLastName.TabIndex = 5;
             // 
             // TextFirstName
             // 
             this.tableLayoutPanel1.SetColumnSpan(this.TextFirstName, 2);
             this.TextFirstName.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.TextFirstName.Location = new System.Drawing.Point(251, 166);
+            this.TextFirstName.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte) (204)));
+            this.TextFirstName.Location = new System.Drawing.Point(215, 148);
             this.TextFirstName.MaxLength = 50;
             this.TextFirstName.Name = "TextFirstName";
-            this.TextFirstName.Size = new System.Drawing.Size(186, 20);
+            this.TextFirstName.Size = new System.Drawing.Size(234, 26);
             this.TextFirstName.TabIndex = 6;
             // 
             // TextSecondName
             // 
             this.tableLayoutPanel1.SetColumnSpan(this.TextSecondName, 2);
             this.TextSecondName.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.TextSecondName.Location = new System.Drawing.Point(251, 192);
+            this.TextSecondName.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte) (204)));
+            this.TextSecondName.Location = new System.Drawing.Point(215, 178);
             this.TextSecondName.MaxLength = 50;
             this.TextSecondName.Name = "TextSecondName";
-            this.TextSecondName.Size = new System.Drawing.Size(186, 20);
+            this.TextSecondName.Size = new System.Drawing.Size(234, 26);
             this.TextSecondName.TabIndex = 7;
             // 
             // TextAge
             // 
             this.tableLayoutPanel1.SetColumnSpan(this.TextAge, 2);
             this.TextAge.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.TextAge.Location = new System.Drawing.Point(251, 218);
+            this.TextAge.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte) (204)));
+            this.TextAge.Location = new System.Drawing.Point(215, 208);
             this.TextAge.MaxLength = 3;
             this.TextAge.Name = "TextAge";
-            this.TextAge.Size = new System.Drawing.Size(186, 20);
+            this.TextAge.Size = new System.Drawing.Size(234, 26);
             this.TextAge.TabIndex = 8;
             this.TextAge.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.TextAge_KeyPress);
             // 
             // ButtonBegin
             // 
             this.ButtonBegin.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.ButtonBegin.Location = new System.Drawing.Point(536, 397);
+            this.ButtonBegin.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte) (204)));
+            this.ButtonBegin.Location = new System.Drawing.Point(572, 415);
             this.ButtonBegin.Margin = new System.Windows.Forms.Padding(0);
             this.ButtonBegin.Name = "ButtonBegin";
-            this.ButtonBegin.Size = new System.Drawing.Size(96, 26);
+            this.ButtonBegin.Size = new System.Drawing.Size(120, 30);
             this.ButtonBegin.TabIndex = 13;
             this.ButtonBegin.Text = "Начать";
             this.ButtonBegin.UseVisualStyleBackColor = true;
@@ -203,9 +210,11 @@ namespace Recovery2.Views
             // 
             // RadioMale
             // 
-            this.RadioMale.Location = new System.Drawing.Point(251, 244);
+            this.RadioMale.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.RadioMale.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte) (204)));
+            this.RadioMale.Location = new System.Drawing.Point(215, 238);
             this.RadioMale.Name = "RadioMale";
-            this.RadioMale.Size = new System.Drawing.Size(90, 20);
+            this.RadioMale.Size = new System.Drawing.Size(114, 24);
             this.RadioMale.TabIndex = 9;
             this.RadioMale.TabStop = true;
             this.RadioMale.Text = "Мужской";
@@ -213,9 +222,11 @@ namespace Recovery2.Views
             // 
             // RadioFemale
             // 
-            this.RadioFemale.Location = new System.Drawing.Point(347, 244);
+            this.RadioFemale.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.RadioFemale.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte) (204)));
+            this.RadioFemale.Location = new System.Drawing.Point(335, 238);
             this.RadioFemale.Name = "RadioFemale";
-            this.RadioFemale.Size = new System.Drawing.Size(90, 20);
+            this.RadioFemale.Size = new System.Drawing.Size(114, 24);
             this.RadioFemale.TabIndex = 10;
             this.RadioFemale.TabStop = true;
             this.RadioFemale.Text = "Женский";
@@ -224,10 +235,11 @@ namespace Recovery2.Views
             // ButtonClear
             // 
             this.ButtonClear.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.ButtonClear.Location = new System.Drawing.Point(152, 397);
+            this.ButtonClear.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte) (204)));
+            this.ButtonClear.Location = new System.Drawing.Point(92, 415);
             this.ButtonClear.Margin = new System.Windows.Forms.Padding(0);
             this.ButtonClear.Name = "ButtonClear";
-            this.ButtonClear.Size = new System.Drawing.Size(96, 26);
+            this.ButtonClear.Size = new System.Drawing.Size(120, 30);
             this.ButtonClear.TabIndex = 11;
             this.ButtonClear.Text = "Очистить";
             this.ButtonClear.UseVisualStyleBackColor = true;
@@ -236,10 +248,11 @@ namespace Recovery2.Views
             // ButtonConfig
             // 
             this.ButtonConfig.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.ButtonConfig.Location = new System.Drawing.Point(536, 137);
+            this.ButtonConfig.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte) (204)));
+            this.ButtonConfig.Location = new System.Drawing.Point(572, 115);
             this.ButtonConfig.Margin = new System.Windows.Forms.Padding(0);
             this.ButtonConfig.Name = "ButtonConfig";
-            this.ButtonConfig.Size = new System.Drawing.Size(96, 26);
+            this.ButtonConfig.Size = new System.Drawing.Size(120, 30);
             this.ButtonConfig.TabIndex = 12;
             this.ButtonConfig.Text = "Параметры";
             this.ButtonConfig.UseVisualStyleBackColor = true;
